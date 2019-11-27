7f283a07efb3bac76af850fd1acac04ba95217e7
Datomo <dave.lengw@gmail.com>
2019-11-27 19:01:44 +0100

    got the port forwarding working

--- code/table-setup/create_pain.py
+++ code/table-setup/create_pain.py
@@ -1,23 +1,38 @@
+from sshtunnel import SSHTunnelForwarder
 import mysql.connector
-import sshtunnel
 
-with sshtunnel.open_tunnel(
-    ('134.209.234.163', 22),
-    ssh_username="root",
-    ssh_password="db_project2019",
-    remote_bind_address=('127.0.0.1', 3306)
-) as tunnel:
-    print("finished")
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="admin",
-        passwd="d7730e905d7c2a007371051d2060cc8d9de4ba9230e7bfc8",
-        db="mysql",
-        port='3306'
-    )
+# ssh variables
+host = '134.209.234.163'
+localhost = '127.0.0.1'
+ssh_username = 'root'
+ssh_password = 'db_project2019'
 
-mycursor = mydb.cursor()
+# database variables
+user='admin'
+password='d7730e905d7c2a007371051d2060cc8d9de4ba9230e7bfc8'
+database='db_project'
 
-mycursor.execute("CREATE DATABASE mydatabase")
 
-tunnel.close()
+def query(q):
+     with SSHTunnelForwarder(
+          (host, 22),
+          ssh_username=ssh_username,
+          ssh_password=ssh_password,
+          remote_bind_address=("localhost", 3306),
+          local_bind_address=("localhost", 3306)
+    ) as tunnel:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user=user,
+            passwd=password,
+            database=database
+        )
+
+        cursor = mydb.cursor()
+        cursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))") 
+        cursor.close()
+        mydb.close()
+        tunnel.stop()
+    
+
+query("")
