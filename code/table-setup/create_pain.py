ca82f01adb00bb66b5a16b81141f493f428c506c
Datomo <dave.lengw@gmail.com>
2019-11-28 12:28:54 +0100

    started with the objectification of the database

--- code/table-setup/create_pain.py
+++ code/table-setup/create_pain.py
@@ -1,5 +1,5 @@
-from sshtunnel import SSHTunnelForwarder
 import mysql.connector
+from sshtunnel import SSHTunnelForwarder
 
 # ssh variables
 host = '134.209.234.163'
@@ -8,31 +8,45 @@ ssh_username = 'root'
 ssh_password = 'db_project2019'
 
 # database variables
-user='admin'
-password='d7730e905d7c2a007371051d2060cc8d9de4ba9230e7bfc8'
-database='db_project'
+user = 'admin'
+password = 'd7730e905d7c2a007371051d2060cc8d9de4ba9230e7bfc8'
+database = 'db_project'
 
 
-def query(q):
-     with SSHTunnelForwarder(
-          (host, 22),
-          ssh_username=ssh_username,
-          ssh_password=ssh_password,
-          remote_bind_address=("localhost", 3306),
-          local_bind_address=("localhost", 3306)
-    ) as tunnel:
-        mydb = mysql.connector.connect(
+class Database:
+    def __init__(self):
+        self.open_connection()
+
+    def open_connection(self):
+        self.tunnel = SSHTunnelForwarder(
+            (host, 22),
+            ssh_username=ssh_username,
+            ssh_password=ssh_password,
+            remote_bind_address=("localhost", 3306),
+            local_bind_address=("localhost", 3306)
+        )
+        self.tunnel.start()
+        self.db = mysql.connector.connect(
             host="localhost",
             user=user,
             passwd=password,
             database=database
         )
 
-        cursor = mydb.cursor()
-        cursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))") 
+    def query(self, query):
+        cursor = self.db.cursor()
+        cursor.execute(query)
         cursor.close()
-        mydb.close()
-        tunnel.stop()
-    
+        self.db.close()
 
-query("")
+    def close_connection(self):
+        self.tunnel.stop()
+
+
+def main():
+    db = Database()
+    db.query("CREATE TABLE test (nummer INTEGER, PRIMARY KEY (nummer))")
+    db.close_connection()
+
+
+main()
