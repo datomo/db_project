77257da5daef901c626c897f819f2f71cb01c653
David Lengweiler <dave.lengw@gmail.com>
2019-12-06 09:42:01 +0100

    added connection pool

--- code/mutliprocessor.py
+++ code/mutliprocessor.py
@@ -1,12 +1,21 @@
 from multiprocessing import Pool
+import mysql.connector
+from mysql.connector import pooling
 import math 
 
+global conpool
+
 def f(inputs):
+    global conpool
+    conpool.get_connection()
     x = [x for x in inputs]
     print(str(x))
 
 if __name__ == '__main__':
-    pool = Pool(processes=4)
+    processes = 4
+    pool = Pool(processes=processes)
+    global conpool
+    conpol = mysql.connector.pooling.MySQLConnectionPool(pool_name="pool", pool_size=processes)
 
     amount = 30
     iter = math.ceil(30/4)
