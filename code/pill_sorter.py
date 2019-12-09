bcbea8f821b2b8e1e38c4aeff1b1d82291b9e459
David Lengweiler <dave.lengw@gmail.com>
2019-12-09 09:54:33 +0100

    fixed folder structure

--- code/pill_sorter.py
+++ code/pill_sorter.py
@@ -1,3 +1,5 @@
+import os
+
 file_path = "../data/arcos_all_washpost.tsv"
 #file_path = "../data/arcos-az-maricopa-04013-itemized.tsv"
 
@@ -5,14 +7,18 @@ class Sorter:
 
     @staticmethod
     def start_sorting(): 
+        file_prefix = "states"
+        if not os.path.exists(file_prefix):
+            os.makedirs(file_prefix)
+
         global file_path   
         with open(file_path, 'r') as file:
 
             for col in file:
                 elements = col.replace("\n", "").split('\t')
                 if elements[7] == elements[17]:
-                    with open(elements[7], "a+") as state_file:
+                    with open(file_prefix + "/" + elements[7] + ".txt", "a+") as state_file:
                         state_file.write(elements[7])
                 else:
-                    with open("else", "a+") as placeholder_file:
+                    with open(file_prefix + "/else.txt", "a+") as placeholder_file:
                         placeholder_file.write(placeholder_file)
